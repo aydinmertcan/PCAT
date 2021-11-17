50c031d267becf584010bd1f56f38ebb37a4f57f
mertcanaydin <ayd.mertcan@gmail.com>
2021-11-17 13:37:40 +0300

    main| Static files are uploded.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-app.get('/',(req, res) => {
-    const photo = {
-        id: 1,
-        name: "photo name",
-        description: "photo description"
-    };
-    res.send(photo);
-})
+const myLogger = (req, res, next) => {
+  console.log("Middleware log 1");
+  next();
+};
+const myLogger2 = (req, res, next) => {
+  console.log("Middleware log 2");
+  next();
+};
+
+// MIDDLEWARES
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "templatemo/index.html"));
+});
 
 const port = 3000;
 app.listen(port, () => {
